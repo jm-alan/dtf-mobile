6d61b14137ed7d72ca99713d92ba9fc659028919
Joshua <j.maxwell.alan@gmail.com>
2021-03-22 00:32:46 -0400

    root reducer

---
+++ assets/store/index.js
@@ -0,0 +1,27 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import session from './session';
+import router from './router';
+import uiController from './uiController';
+
+const rootReducer = combineReducers({
+  router,
+  uiController,
+  session
+});
+
+let enhancer;
+
+if (process.env.NODE_ENV === 'production') {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = require('redux-logger').default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+export default function configureStore (preloadedState) {
+  return createStore(rootReducer, preloadedState, enhancer);
+}
