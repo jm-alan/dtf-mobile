37c77a42297e98d3e57baf59426328c4ab302627
Joshua <j.maxwell.alan@gmail.com>
2021-03-22 00:32:22 -0400

    basic user state reducer

---
+++ assets/store/session.js
@@ -0,0 +1,65 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import csrfetch from './csrf.js';
+
+const USER = 'session/USER';
+
+const LOAD = 'session/LOAD';
+
+const UNLOAD = 'session/UNLOAD';
+
+export const SetSession = (user = null) => ({ type: USER, user });
+
+export const LoadSession = () => ({ type: LOAD });
+
+export const UnloadSession = () => ({ type: UNLOAD });
+
+export const RestoreUser = () => async dispatch => {
+  const { data: { user } } = await csrfetch('/api/session');
+  dispatch(SetSession(user));
+};
+
+export const LogIn = (identification, password) => async dispatch => {
+  const { data: { user, token } } = await csrfetch('/api/session', {
+    method: 'POST',
+    body: JSON.stringify({ identification, password })
+  });
+  await AsyncStorage.setItem('JWT', token);
+  dispatch(SetSession(user));
+};
+
+export const SignUp = newUser => async dispatch => {
+  const { firstName, email, password } = newUser;
+  const { data: { user, token } } = await csrfetch('/api/users', {
+    method: 'POST',
+    body: JSON.stringify({
+      firstName,
+      email,
+      password
+    })
+  });
+  await AsyncStorage.setItem('JWT', token);
+  dispatch(SetSession(user));
+};
+
+export const LogOut = () => async dispatch => {
+  await csrfetch('/api/session', {
+    method: 'DELETE'
+  });
+  dispatch(SetSession());
+};
+
+export default function reducer (
+  state = { user: null, loaded: false },
+  { type, user }
+) {
+  switch (type) {
+    case USER:
+      return { ...state, user };
+    case LOAD:
+      return { ...state, loaded: true };
+    case UNLOAD:
+      return { ...state, loaded: false };
+    default:
+      return state;
+  }
+}
