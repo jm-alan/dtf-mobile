aa2ab4aa89f31572d5641e627ae13fc2b8fc34d9
Joshua <j.maxwell.alan@gmail.com>
2021-03-22 00:33:51 -0400

    skeleton testing

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Dimensions, SafeAreaView } from 'react-native';
+import { Provider } from 'react-redux';
 
-export default function App() {
+import Home from './assets/components/Home';
+import Sidebar from './assets/components/Sidebar';
+
+import configureStore from './assets/store/index.js';
+// import { getCSRFtoken } from './assets/store/csrf';
+
+const store = configureStore();
+
+const { width, height } = Dimensions.get('window');
+
+export default function App () {
+  useEffect(() => {
+    // getCSRFtoken();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <SafeAreaView style={{
+        width,
+        height,
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center'
+      }}
+      >
+        <Sidebar />
+        <Home />
+      </SafeAreaView>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
