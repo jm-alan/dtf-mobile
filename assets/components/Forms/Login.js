3760373e23503d65df0f900d7783ad3c5cb170ed
Joshua <j.maxwell.alan@gmail.com>
2021-03-22 00:33:04 -0400

    dummy forms

---
+++ assets/components/Forms/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Login () {
+  return (
+    <View>
+      <Text>This will be the login page!</Text>
+    </View>
+  );
+}
---
+++ assets/components/Forms/Signup.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Signup () {
+  return (
+    <View>
+      <Text>This will be the signup page!</Text>
+    </View>
+  );
+}
