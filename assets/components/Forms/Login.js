42eba34e3135ec3cb85e23c33f27965e4acc462e
Joshua <j.maxwell.alan@gmail.com>
2021-03-26 14:46:00 -0400

    Login worksclear && git add Login.js clear && git add Login.js

--- assets/components/Forms/Login.js
+++ assets/components/Forms/Login.js
@@ -1,10 +1,79 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { StyleSheet, Dimensions, Text, View, TextInput } from 'react-native';
+
+import * as Session from '../../store/session';
+import * as Router from '../../store/router';
+
+const { width } = Dimensions.get('window');
 
 export default function Login () {
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.session.user);
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [showError, setShowError] = useState(false);
+
+  const passRef = useRef(null);
+
+  const onSubmit = () => {
+    setShowError(false);
+    dispatch(Session.LogIn(username, password))
+      .catch(() => setShowError(true));
+  };
+
+  if (user) {
+    dispatch(Router.GoHome());
+    return null;
+  }
+
   return (
-    <View>
-      <Text>This will be the login page!</Text>
+    <View style={styles.container}>
+      {showError ? <Text>Invalid Username or Password</Text> : null}
+      <Text style={styles.formHeading}>
+        Login
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setUsername}
+        value={username}
+        autoCorrect={false}
+        blurOnSubmit={false}
+        returnKeyType='next'
+        onSubmitEditing={() => passRef.current.focus()}
+      />
+      <TextInput
+        ref={passRef}
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        onSubmitEditing={onSubmit}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  formHeading: {
+    fontSize: 100
+  },
+  input: {
+    textAlign: 'center',
+    borderColor: 'darkgrey',
+    borderWidth: 1,
+    fontSize: 30,
+    height: 60,
+    width: width * 0.85
+  },
+  error: {
+    fontSize: 30,
+    color: 'red'
+  }
+});
