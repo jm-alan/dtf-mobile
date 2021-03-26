7eba6175637aac0682513c57a9c50fa355b65eff
Joshua <j.maxwell.alan@gmail.com>
2021-03-26 15:14:47 -0400

    create easy sidebar toggle for menu bar

--- assets/store/uiController.js
+++ assets/store/uiController.js
@@ -2,9 +2,13 @@ const SIDEBAR = 'interface/SIDEBAR';
 
 const HIDEBAR = 'interface/HIDEBAR';
 
+const TOGGLEBAR = 'interface/TOGGLEBAR';
+
 export const ShowSidebar = () => ({ type: SIDEBAR });
 
 export const HideSidebar = () => ({ type: HIDEBAR });
+
+export const ToggleSidebar = () => ({ type: TOGGLEBAR });
 
 export default function reducer (state = { sidebar: false }, { type }) {
   switch (type) {
@@ -12,6 +16,8 @@ export default function reducer (state = { sidebar: false }, { type }) {
       return { ...state, sidebar: true };
     case HIDEBAR:
       return { ...state, sidebar: false };
+    case TOGGLEBAR:
+      return { ...state, sidebar: !state.sidebar };
     default:
       return state;
   }
