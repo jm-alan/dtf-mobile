848c28bac2648e6cb0faaec6c5ce1f504a897d21
Joshua <j.maxwell.alan@gmail.com>
2021-03-26 14:46:41 -0400

    work in progress

--- assets/components/Forms/Signup.js
+++ assets/components/Forms/Signup.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function Signup () {
   return (
-    <View>
-      <Text>This will be the signup page!</Text>
+    <View style={styles.container}>
+      <Text style={styles.formHeading}>
+        Signup
+      </Text>
+      <TextInput />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  formHeading: {
+    fontSize: 100
+  }
+});
